774bd8d56b49669c3d26fd4e668919ba2bca2844
Yair G <yjgarizabalo@gmail.com>
2023-04-28 16:33:42 -0500

    [prueba_tecnoglass] add ✅

--- app.py
+++ app.py
@@ -24,145 +24,145 @@ consultas = conexion.cursor()
 # print ('Tabla de usuarios creada')
 
 # CREAR USUARIO
-# @app.route('/usuario', methods=['POST'])
-# def usuario():
-#   CEDULA = request.json['CEDULA']
-#   NOMBRE = request.json['NOMBRE']
-#   DIRECCION = request.json['DIRECCION']
-#   TELEFONO = request.json['TELEFONO']
-#   NACIONALIDAD = request.json['NACIONALIDAD']
-#   CORREO = request.json['CORREO']
+@app.route('/usuario', methods=['POST'])
+def usuario():
+  CEDULA = request.json['CEDULA']
+  NOMBRE = request.json['NOMBRE']
+  DIRECCION = request.json['DIRECCION']
+  TELEFONO = request.json['TELEFONO']
+  NACIONALIDAD = request.json['NACIONALIDAD']
+  CORREO = request.json['CORREO']
 
-#   consultas.execute("SELECT * FROM usuarios WHERE CEDULA =?", (CEDULA,))
-#   usuarios_existe = consultas.fetchone()
-#   if usuarios_existe:
-#     return jsonify({'mensaje': "El usuario ya fue creado ⚠️"}), 400
+  consultas.execute("SELECT * FROM usuarios WHERE CEDULA =?", (CEDULA,))
+  usuarios_existe = consultas.fetchone()
+  if usuarios_existe:
+    return jsonify({'mensaje': "El usuario ya fue creado ⚠️"}), 400
   
-#   consultas.execute("INSERT INTO usuarios (CEDULA, NOMBRE, DIRECCION, TELEFONO, NACIONALIDAD, CORREO) VALUES(?, ?, ?, ?, ?, ?)", (CEDULA, NOMBRE, DIRECCION, TELEFONO, NACIONALIDAD, CORREO))
+  consultas.execute("INSERT INTO usuarios (CEDULA, NOMBRE, DIRECCION, TELEFONO, NACIONALIDAD, CORREO) VALUES(?, ?, ?, ?, ?, ?)", (CEDULA, NOMBRE, DIRECCION, TELEFONO, NACIONALIDAD, CORREO))
 
-#   conexion.commit()
-#   return jsonify({'mensaje': "Usuario Creado exitosamente ✅"}), 200
+  conexion.commit()
+  return jsonify({'mensaje': "Usuario Creado exitosamente ✅"}), 200
 
-# # LISTAR USUARIOS
-# @app.route('/listar', methods=['GET'])
-# def listar():
-#   consultas.execute("SELECT * FROM usuarios")
-#   listar_usarios = consultas.fetchall()
-#   return jsonify(listar_usarios)
+# LISTAR USUARIOS
+@app.route('/listar', methods=['GET'])
+def listar():
+  consultas.execute("SELECT * FROM usuarios")
+  listar_usarios = consultas.fetchall()
+  return jsonify(listar_usarios)
 
-# # LISTAR USUARIO POR CEDULA | NOMBRE
-# @app.route('/listar_usario',  methods=['POST'])
-# def listar_usario():
-#   CEDULA = request.json['CEDULA']
-#   NOMBRE = request.json['NOMBRE']
+# LISTAR USUARIO POR CEDULA | NOMBRE
+@app.route('/listar_usario',  methods=['POST'])
+def listar_usario():
+  CEDULA = request.json['CEDULA']
+  NOMBRE = request.json['NOMBRE']
 
-#   consultas.execute("SELECT * FROM usuarios WHERE CEDULA=? OR NOMBRE=?", (CEDULA, NOMBRE))
-#   lista = consultas.fetchall()
-#   if not bool(lista):
-#     return jsonify({"mensaje": "No se encontraron resultados ❌"}), 400
-#   else:
-#     return jsonify(lista)
+  consultas.execute("SELECT * FROM usuarios WHERE CEDULA=? OR NOMBRE=?", (CEDULA, NOMBRE))
+  lista = consultas.fetchall()
+  if not bool(lista):
+    return jsonify({"mensaje": "No se encontraron resultados ❌"}), 400
+  else:
+    return jsonify(lista)
 
-# # ACTULIZAR USUARIO
-# @app.route('/actualiar_usario', methods=['PUT'])
-# def actuliar_usario():
-#   ID = request.json['ID']
-#   CEDULA = request.json['CEDULA']
-#   NOMBRE = request.json['NOMBRE']
-#   DIRECCION = request.json['DIRECCION']
-#   TELEFONO = request.json['TELEFONO']
-#   NACIONALIDAD = request.json['NACIONALIDAD']
-#   CORREO = request.json['CORREO']
+# ACTULIZAR USUARIO
+@app.route('/actualiar_usario', methods=['PUT'])
+def actuliar_usario():
+  ID = request.json['ID']
+  CEDULA = request.json['CEDULA']
+  NOMBRE = request.json['NOMBRE']
+  DIRECCION = request.json['DIRECCION']
+  TELEFONO = request.json['TELEFONO']
+  NACIONALIDAD = request.json['NACIONALIDAD']
+  CORREO = request.json['CORREO']
 
-#   consultas.execute("SELECT * FROM usuarios WHERE CEDULA=?", (CEDULA,))
-#   usuario_existe = consultas.fetchone()
+  consultas.execute("SELECT * FROM usuarios WHERE CEDULA=?", (CEDULA,))
+  usuario_existe = consultas.fetchone()
 
-#   if usuario_existe is not None:
-#     return jsonify({'mensaje': "Usuario ya existe ❌"}), 400
-#   else:
-#       consultas.execute("UPDATE usuarios SET CEDULA=?, NOMBRE=?, DIRECCION=?, TELEFONO=?, NACIONALIDAD=?, CORREO=? WHERE ID=?", (CEDULA, NOMBRE, DIRECCION, TELEFONO, NACIONALIDAD, CORREO, ID))
-#       conexion.commit()
-#       return jsonify({'mensaje': 'Usuario actulizado correctamente ✅'})
+  if usuario_existe is not None:
+    return jsonify({'mensaje': "Usuario ya existe ❌"}), 400
+  else:
+      consultas.execute("UPDATE usuarios SET CEDULA=?, NOMBRE=?, DIRECCION=?, TELEFONO=?, NACIONALIDAD=?, CORREO=? WHERE ID=?", (CEDULA, NOMBRE, DIRECCION, TELEFONO, NACIONALIDAD, CORREO, ID))
+      conexion.commit()
+      return jsonify({'mensaje': 'Usuario actulizado correctamente ✅'})
   
 
-# # ELIMINAR USIARIO
-# @app.route('/eliminar_usuario', methods=['DELETE'])
-# def eliminar_usuario():
-#   ID = request.json['ID']
-#   consultas.execute("DELETE FROM usuarios WHERE ID=?", (ID,)).fetchone()
+# ELIMINAR USIARIO
+@app.route('/eliminar_usuario', methods=['DELETE'])
+def eliminar_usuario():
+  ID = request.json['ID']
+  consultas.execute("DELETE FROM usuarios WHERE ID=?", (ID,)).fetchone()
 
-#   conexion.commit()
-#   return jsonify({'mensaje': "usuario eliminado con exito"})
+  conexion.commit()
+  return jsonify({'mensaje': "usuario eliminado con exito"})
 
 
-# # ORDEN DE VIDRIOS DE UN CLIENTE
+# ORDEN DE VIDRIOS DE UN CLIENTE
 
-# # TABLA DE ORDENES
-# # conexion.execute('''
-# #                 CREATE TABLE IF NOT EXISTS ordenes
-# #                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
-# #                 NUMERO_ORDEN TEXT NOT NULL,
-# #                 CLIENTE INTEGER NOT NULL,
-# #                 FECHA_ORDEN TEXT NOT NULL,
-# #                 ESTADO TEXT NOT NULL DEFAULT 'SOLICITADO',
-# #                 FOREIGN KEY(CLIENTE) REFERENCES usarios(ID));
-# #                 ''')
+# TABLA DE ORDENES
+# conexion.execute('''
+#                 CREATE TABLE IF NOT EXISTS ordenes
+#                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                 NUMERO_ORDEN TEXT NOT NULL,
+#                 CLIENTE INTEGER NOT NULL,
+#                 FECHA_ORDEN TEXT NOT NULL,
+#                 ESTADO TEXT NOT NULL DEFAULT 'SOLICITADO',
+#                 FOREIGN KEY(CLIENTE) REFERENCES usarios(ID));
+#                 ''')
 
-# # print ('Tabla de Orden creada')
+# print ('Tabla de Orden creada')
 
-# # INGRESAR UNA ORDEN
-# @app.route('/orden', methods=['POST'])
-# def orden():
-#   NUMERO_ORDEN = request.json['NUMERO_ORDEN']
-#   CLIENTE = request.json['CLIENTE']
-#   FECHA_ORDEN = request.json['FECHA_ORDEN']
+# INGRESAR UNA ORDEN
+@app.route('/orden', methods=['POST'])
+def orden():
+  NUMERO_ORDEN = request.json['NUMERO_ORDEN']
+  CLIENTE = request.json['CLIENTE']
+  FECHA_ORDEN = request.json['FECHA_ORDEN']
 
-#   consultas.execute("SELECT * FROM ordenes WHERE NUMERO_ORDEN=?", (NUMERO_ORDEN,))
-#   orden_existe = consultas.fetchone()
+  consultas.execute("SELECT * FROM ordenes WHERE NUMERO_ORDEN=?", (NUMERO_ORDEN,))
+  orden_existe = consultas.fetchone()
 
-#   if orden_existe: 
-#     return jsonify({'mensaje': "Numero de orden ya existe ⚠️"}), 400
+  if orden_existe: 
+    return jsonify({'mensaje': "Numero de orden ya existe ⚠️"}), 400
 
-#   consultas.execute("INSERT INTO ordenes (NUMERO_ORDEN, CLIENTE, FECHA_ORDEN) VALUES (?, ?, ?)", (NUMERO_ORDEN, CLIENTE, FECHA_ORDEN))
+  consultas.execute("INSERT INTO ordenes (NUMERO_ORDEN, CLIENTE, FECHA_ORDEN) VALUES (?, ?, ?)", (NUMERO_ORDEN, CLIENTE, FECHA_ORDEN))
 
-#   conexion.commit()
-#   return jsonify({'mensaje': "Orden creada con existo ✅"}), 200
+  conexion.commit()
+  return jsonify({'mensaje': "Orden creada con existo ✅"}), 200
 
-# # ACUTLIZAR EL ESTADO DE UNA ORDEN
-# @app.route('/actulizar_orden', methods=['PUT'])
-# def actulizar_orden():
+# ACUTLIZAR EL ESTADO DE UNA ORDEN
+@app.route('/actulizar_orden', methods=['PUT'])
+def actulizar_orden():
 
-#   NUMERO_ORDEN = request.json['NUMERO_ORDEN']
-#   ESTADO = request.json['ESTADO']
+  NUMERO_ORDEN = request.json['NUMERO_ORDEN']
+  ESTADO = request.json['ESTADO']
 
-#   consultas.execute("SELECT COUNT(*) FROM ordenes WHERE NUMERO_ORDEN=?", (NUMERO_ORDEN,))
-#   orden_existe = consultas.fetchone()[0] == 0
-#   if orden_existe:
-#     return jsonify({'mensaje': "Numero de orden no existe"}), 400
+  consultas.execute("SELECT COUNT(*) FROM ordenes WHERE NUMERO_ORDEN=?", (NUMERO_ORDEN,))
+  orden_existe = consultas.fetchone()[0] == 0
+  if orden_existe:
+    return jsonify({'mensaje': "Numero de orden no existe"}), 400
 
-#   if ESTADO == "APROBADA":
-#     consultas.execute("UPDATE ordenes SET ESTADO =? WHERE NUMERO_ORDEN=?", (ESTADO, NUMERO_ORDEN))
-#     conexion.commit()
-#     return  jsonify({'mensaje': "Oreden aprobada con exito ✅"}), 200
-#   elif ESTADO == "ANULADA":
-#     consultas.execute("UPDATE ordenes SET ESTADO =? WHERE NUMERO_ORDEN=?", (ESTADO, NUMERO_ORDEN))   
-#     conexion.commit()
-#     return jsonify({'mensaje': "Oreden anulada ⚠️"}), 200
-#   else:
-#     return jsonify({'mensaje': "Ingresar un estado correcto"}), 400
+  if ESTADO == "APROBADA":
+    consultas.execute("UPDATE ordenes SET ESTADO =? WHERE NUMERO_ORDEN=?", (ESTADO, NUMERO_ORDEN))
+    conexion.commit()
+    return  jsonify({'mensaje': "Oreden aprobada con exito ✅"}), 200
+  elif ESTADO == "ANULADA":
+    consultas.execute("UPDATE ordenes SET ESTADO =? WHERE NUMERO_ORDEN=?", (ESTADO, NUMERO_ORDEN))   
+    conexion.commit()
+    return jsonify({'mensaje': "Oreden anulada ⚠️"}), 200
+  else:
+    return jsonify({'mensaje': "Ingresar un estado correcto"}), 400
 
 
 #TABLA DETALLE DE ORDENES
-conexion.execute('''
-                CREATE TABLE IF NOT EXISTS detalles_orden
-                (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                ID_ORDEN TEXT NOT NULL,
-                DESCRIPCION TEXT NOT NULL,
-                CANTIDAD INTEGER NOT NULL DEFAULT 0,
-                PRECIO INTEGER NOT NULL DEFAULT 1,
-                TOTAL INTEGER NULL,
-                FOREIGN KEY(ID_ORDEN) REFERENCES ordenes(ID));
-                ''')
+# conexion.execute('''
+#                 CREATE TABLE IF NOT EXISTS detalles_orden
+#                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                 ID_ORDEN TEXT NOT NULL,
+#                 DESCRIPCION TEXT NOT NULL,
+#                 CANTIDAD INTEGER NOT NULL DEFAULT 0,
+#                 PRECIO INTEGER NOT NULL DEFAULT 1,
+#                 TOTAL INTEGER NULL,
+#                 FOREIGN KEY(ID_ORDEN) REFERENCES ordenes(ID));
+#                 ''')
 
 print ('Tabla de detalle orden creada')
 
